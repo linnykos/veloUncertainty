1497150a74f5a6e7453955d1c491a05aad188b3f
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-02-19 15:45:38 -0800

    data preprocess

--- code/yuhong/scvelo-test.py
+++ code/yuhong/scvelo-test.py
@@ -18,5 +18,9 @@ scv.pp.moments(adata2000, n_pcs=30, n_neighbors=30)
 # Using dynamical model (see paper) to calculate gene velocity
 scv.tl.recover_dynamics(adata2000, n_jobs=8)
 scv.tl.velocity(adata2000, mode='dynamical')
+scv.tl.velocity_graph(adata2000, n_jobs=8)
 
 adata2000.write_h5ad(filename = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/Writeup1/scvelo-test.h5ad")
+
+# to read in this file, run the following line:
+# adata2000 = anndata.read_h5ad('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/Writeup1/scvelo-test.h5ad')
