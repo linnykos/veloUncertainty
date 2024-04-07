0adc80f7dc900c17e2e3205167d89e7e57930dfe
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-04-06 17:58:31 -0700

    umap bbknn

--- code/yuhong/erythroid/1_countsplit_and_umap/erythroid_umap_bbknn.py
+++ code/yuhong/erythroid/1_countsplit_and_umap/erythroid_umap_bbknn.py
@@ -1,134 +1,47 @@
 import scvelo as scv
 import scanpy as sc
-import bbknn
 
 ## total (the same for different seeds, so just use one of them)
-### uncorrected
-adata_total = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_total_seurat_seed317.h5ad')
-scv.pp.normalize_per_cell(adata_total)
-scv.pp.log1p(adata_total)
-scv.pp.moments(adata_total, n_pcs=30, n_neighbors=30)
-sc.tl.pca(adata_total)
-sc.pp.neighbors(adata_total, n_neighbors=10, n_pcs=40)
-sc.tl.umap(adata_total)
-scv.tl.recover_dynamics(adata_total)
-scv.tl.velocity(adata_total, mode="dynamical")
-scv.tl.velocity_graph(adata_total)
+## batch corrected
+adata_total = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_total_seurat_seed317_bbknn.h5ad')
 scv.pl.velocity_embedding_stream(adata_total, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_total_uncorrected_bbknn_celltype.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_total_bbknn_celltype.png")
 scv.pl.velocity_embedding_stream(adata_total, basis='umap',color="sequencing.batch",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_total_uncorrected_bbknn_seqbat.png")
-
-### batch corrected
-adata_total = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_total_seurat_seed317.h5ad')
-scv.pp.normalize_per_cell(adata_total)
-scv.pp.log1p(adata_total)
-scv.pp.moments(adata_total, n_pcs=30, n_neighbors=30)
-### batch correction
-bbknn.bbknn(adata_total, batch_key='sequencing.batch')
-adata_total.X = adata_total.X.toarray()
-bbknn.ridge_regression(adata_total, batch_key='sample', confounder_key='celltype')
-sc.tl.pca(adata_total)
-bbknn.bbknn(adata_total, batch_key='sequencing.batch')
-print("Batch correction done for total counts!")
-sc.pp.neighbors(adata_total, n_neighbors=10, n_pcs=40)
-sc.tl.umap(adata_total)
-scv.tl.recover_dynamics(adata_total)
-scv.tl.velocity(adata_total, mode="dynamical")
-scv.tl.velocity_graph(adata_total)
-scv.pl.velocity_embedding_stream(adata_total, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_total_bbknn_celltype.png")
-scv.pl.velocity_embedding_stream(adata_total, basis='umap',color="sequencing.batch",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_total_bbknn_seqbat.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_total_bbknn_seqbat.png")
+print("**************** total counts done ****************")
 
 ## split1
 ### seed317
-adata_split1 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split1_seurat_seed317.h5ad")
-scv.pp.normalize_per_cell(adata_split1)
-scv.pp.log1p(adata_split1)
-scv.pp.moments(adata_split1, n_pcs=30, n_neighbors=30)
-### batch correction
-bbknn.bbknn(adata_split1, batch_key='sequencing.batch')
-adata_split1.X = adata_split1.X.toarray()
-bbknn.ridge_regression(adata_split1, batch_key='sample', confounder_key='celltype')
-sc.tl.pca(adata_split1)
-bbknn.bbknn(adata_split1, batch_key='sequencing.batch')
-print("Batch correction done for seed317 split1 counts!")
-sc.pp.neighbors(adata_split1, n_neighbors=10, n_pcs=40)
-sc.tl.umap(adata_split1)
-scv.tl.recover_dynamics(adata_split1)
-scv.tl.velocity(adata_split1, mode="dynamical")
-scv.tl.velocity_graph(adata_split1)
+adata_split1 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split1_seurat_seed317_bbknn.h5ad")
 scv.pl.velocity_embedding_stream(adata_split1, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split1_seed317_bbknn_celltype.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split1_seed317_bbknn_celltype.png")
 scv.pl.velocity_embedding_stream(adata_split1, basis='umap',color="sequencing.batch",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split1_seed317_bbknn_seqbat.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split1_seed317_bbknn_seqbat.png")
+print("**************** seed317 split1 done ****************")
 
 ### seed320
-adata_split1 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split1_seurat_seed320.h5ad")
-scv.pp.normalize_per_cell(adata_split1)
-scv.pp.log1p(adata_split1)
-scv.pp.moments(adata_split1, n_pcs=30, n_neighbors=30)
-### batch correction
-bbknn.bbknn(adata_split1, batch_key='sequencing.batch')
-adata_split1.X = adata_split1.X.toarray()
-bbknn.ridge_regression(adata_split1, batch_key='sample', confounder_key='celltype')
-sc.tl.pca(adata_split1)
-bbknn.bbknn(adata_split1, batch_key='sequencing.batch')
-print("Batch correction done for seed320 split1 counts!")
-sc.pp.neighbors(adata_split1, n_neighbors=10, n_pcs=40)
-sc.tl.umap(adata_split1)
-scv.tl.recover_dynamics(adata_split1)
-scv.tl.velocity(adata_split1, mode="dynamical")
-scv.tl.velocity_graph(adata_split1)
+adata_split1 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split1_seurat_seed320_bbknn.h5ad")
 scv.pl.velocity_embedding_stream(adata_split1, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split1_seed320_bbknn_celltype.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split1_seed320_bbknn_celltype.png")
 scv.pl.velocity_embedding_stream(adata_split1, basis='umap',color="sequencing.batch",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split1_seed320_bbknn_seqbat.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split1_seed320_bbknn_seqbat.png")
+print("**************** seed320 split1 done ****************")
 
 
 ## split2
 ### seed317
-adata_split2 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split2_seurat_seed317.h5ad")
-scv.pp.normalize_per_cell(adata_split2)
-scv.pp.log1p(adata_split2)
-scv.pp.moments(adata_split2, n_pcs=30, n_neighbors=30)
-### batch correction
-bbknn.bbknn(adata_split2, batch_key='sequencing.batch')
-adata_split2.X = adata_split2.X.toarray()
-bbknn.ridge_regression(adata_split2, batch_key='sample', confounder_key='celltype')
-sc.tl.pca(adata_split2)
-bbknn.bbknn(adata_split2, batch_key='sequencing.batch')
-print("Batch correction done for seed317 split2 counts!")
-sc.pp.neighbors(adata_split2, n_neighbors=10, n_pcs=40)
-sc.tl.umap(adata_split2)
-scv.tl.recover_dynamics(adata_split2)
-scv.tl.velocity(adata_split2, mode="dynamical")
-scv.tl.velocity_graph(adata_split2)
+adata_split2 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split2_seurat_seed317_bbknn.h5ad")
 scv.pl.velocity_embedding_stream(adata_split2, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split2_seed317_bbknn_celltype.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split2_seed317_bbknn_celltype.png")
 scv.pl.velocity_embedding_stream(adata_split2, basis='umap',color="sequencing.batch",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split2_seed317_bbknn_seqbat.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split2_seed317_bbknn_seqbat.png")
+print("**************** seed317 split2 done ****************")
 
 ### seed320
-adata_split2 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split2_seurat_seed320.h5ad")
-scv.pp.normalize_per_cell(adata_split2)
-scv.pp.log1p(adata_split2)
-scv.pp.moments(adata_split2, n_pcs=30, n_neighbors=30)
-### batch correction
-bbknn.bbknn(adata_split2, batch_key='sequencing.batch')
-adata_split2.X = adata_split2.X.toarray()
-bbknn.ridge_regression(adata_split2, batch_key='sample', confounder_key='celltype')
-sc.tl.pca(adata_split2)
-bbknn.bbknn(adata_split2, batch_key='sequencing.batch')
-print("Batch correction done for seed320 split2 counts!")
-sc.pp.neighbors(adata_split2, n_neighbors=10, n_pcs=40)
-sc.tl.umap(adata_split2)
-scv.tl.recover_dynamics(adata_split2)
-scv.tl.velocity(adata_split2, mode="dynamical")
-scv.tl.velocity_graph(adata_split2)
+adata_split2 = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split2_seurat_seed320_bbknn.h5ad")
 scv.pl.velocity_embedding_stream(adata_split2, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split2_seed320_bbknn_celltype.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split2_seed320_bbknn_celltype.png")
 scv.pl.velocity_embedding_stream(adata_split2, basis='umap',color="sequencing.batch",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_erythroid_split2_seed320_bbknn_seqbat.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/bbknn/scvelo_erythroid_split2_seed320_bbknn_seqbat.png")
+print("**************** seed320 split2 done ****************")
 
