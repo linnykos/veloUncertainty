44947a6d23ca0ee57586989e99645b7775589bd3
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-04-06 20:14:02 -0700

    edit saveRdata file

--- code/yuhong/erythroid/1_countsplit_and_umap/erythroid_save_split_RData.R
+++ code/yuhong/erythroid/1_countsplit_and_umap/erythroid_save_split_RData.R
@@ -4,10 +4,10 @@ library(SeuratObject)
 
 setwd("~/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split")
 
-split1_seed317 <- LoadH5Seurat("./scvelo_erythroid_split1_seurat_seed317.h5Seurat")
-split2_seed317 <- LoadH5Seurat("./scvelo_erythroid_split2_seurat_seed317.h5Seurat")
-split1_seed320 <- LoadH5Seurat("./scvelo_erythroid_split1_seurat_seed320.h5Seurat")
-split2_seed320 <- LoadH5Seurat("./scvelo_erythroid_split2_seurat_seed320.h5Seurat")
+split1_seed317 <- LoadH5Seurat("./scvelo_erythroid_split1_seurat_seed317_Rbbknn.h5Seurat")
+split2_seed317 <- LoadH5Seurat("./scvelo_erythroid_split2_seurat_seed317_Rbbknn.h5Seurat")
+split1_seed320 <- LoadH5Seurat("./scvelo_erythroid_split1_seurat_seed320_Rbbknn.h5Seurat")
+split2_seed320 <- LoadH5Seurat("./scvelo_erythroid_split2_seurat_seed320_Rbbknn.h5Seurat")
 
 ## extract spliced and unspliced matrices, stored in list
 spliced_seed317 <- list(SeuratObject::LayerData(split1_seed317,assay="spliced"),
