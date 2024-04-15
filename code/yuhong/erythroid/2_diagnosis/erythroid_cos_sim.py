e45b8bfd09931fbcb5227789f3305efac46a80ac
y2564li <y2564li@biostat-bayes0.biostat.washington.edu>
2024-04-15 14:22:10 -0700

    cosine similarity code bug fixed

--- code/yuhong/erythroid/2_diagnosis/erythroid_cos_sim.py
+++ code/yuhong/erythroid/2_diagnosis/erythroid_cos_sim.py
@@ -9,7 +9,7 @@ adata_split2_seed317 = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncer
 adata_total = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_total_seurat_seed317_bbknn.h5ad')
 
 # compute cosine similarity
-n_cell = adata_split1_seed317.layers["velocity"].shape[1]
+n_cell = adata_split1_seed317.layers["velocity"].shape[0]
 adata_split1_seed317.layers["velocity_rmNA"] = np.nan_to_num(adata_split1_seed317.layers['velocity'], nan=0)
 adata_split2_seed317.layers["velocity_rmNA"] = np.nan_to_num(adata_split2_seed317.layers['velocity'], nan=0)
 cos_sim = cosine_similarity(adata_split1_seed317.layers["velocity_rmNA"],
@@ -25,7 +25,7 @@ adata_split1_seed320 = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncer
 adata_split2_seed320 = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/scvelo_erythroid_split1_seurat_seed320_bbknn.h5ad')
 
 # compute cosine similarity
-n_cell = adata_split1_seed320.layers["velocity"].shape[1]
+n_cell = adata_split1_seed320.layers["velocity"].shape[0]
 adata_split1_seed320.layers["velocity_rmNA"] = np.nan_to_num(adata_split1_seed320.layers['velocity'], nan=0)
 adata_split2_seed320.layers["velocity_rmNA"] = np.nan_to_num(adata_split2_seed320.layers['velocity'], nan=0)
 cos_sim = cosine_similarity(adata_split1_seed320.layers["velocity_rmNA"],
