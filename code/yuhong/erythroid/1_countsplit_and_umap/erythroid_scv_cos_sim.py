2e325ef3dbe1564a0b8acdab5e452e4369d08d7f
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-04-18 21:25:42 -0700

    update batch correction

--- code/yuhong/erythroid/1_countsplit_and_umap/erythroid_scv_cos_sim.py
+++ code/yuhong/erythroid/1_countsplit_and_umap/erythroid_scv_cos_sim.py
@@ -3,6 +3,7 @@ import scanpy as sc
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
+import bbknn
 
 adata_split1_seed317 = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/erythroid_seed317_split1_seurat.h5ad')
 adata_split2_seed317 = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/erythroid_split/erythroid_seed317_split2_seurat.h5ad')
@@ -11,28 +12,40 @@ adata_total = scv.read('/home/users/y2564li/kzlinlab/projects/veloUncertainty/ou
 scv.pp.normalize_per_cell(adata_split1_seed317)
 scv.pp.log1p(adata_split1_seed317)
 scv.pp.moments(adata_split1_seed317, n_pcs=30, n_neighbors=30)
-sc.tl.pca(adata_split1_seed317, svd_solver="arpack")
+## batch correction
+bbknn.bbknn(adata_split1_seed317, batch_key='sequencing.batch')
+adata_split1_seed317.X = adata_split1_seed317.X.toarray()
+bbknn.ridge_regression(adata_split1_seed317, batch_key='sample', confounder_key='celltype')
+sc.tl.pca(adata_split1_seed317)
+bbknn.bbknn(adata_split1_seed317, batch_key='sequencing.batch')
+print("Batch correction for seed317 split1 done!")
 sc.pp.neighbors(adata_split1_seed317, n_neighbors=10, n_pcs=40)
 sc.tl.umap(adata_split1_seed317)
 scv.tl.recover_dynamics(adata_split1_seed317)
 scv.tl.velocity(adata_split1_seed317, mode="dynamical")
 scv.tl.velocity_graph(adata_split1_seed317)
 scv.pl.velocity_embedding_stream(adata_split1_seed317, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/scvelo_seed317_split1_2000.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_seed317_split1_2000.png")
 print("**************** seed317 split1 processed! ****************")
 
 
 scv.pp.normalize_per_cell(adata_split2_seed317)
 scv.pp.log1p(adata_split2_seed317)
 scv.pp.moments(adata_split2_seed317, n_pcs=30, n_neighbors=30)
-sc.tl.pca(adata_split2_seed317, svd_solver="arpack")
+### batch correction
+bbknn.bbknn(adata_split2_seed317, batch_key='sequencing.batch')
+adata_split2_seed317.X = adata_split2_seed317.X.toarray()
+bbknn.ridge_regression(adata_split2_seed317, batch_key='sample', confounder_key='celltype')
+sc.tl.pca(adata_split2_seed317)
+bbknn.bbknn(adata_split2_seed317, batch_key='sequencing.batch')
+print("Batch correction done for seed317 split2 total counts!")
 sc.pp.neighbors(adata_split2_seed317, n_neighbors=10, n_pcs=40)
 sc.tl.umap(adata_split2_seed317)
 scv.tl.recover_dynamics(adata_split2_seed317)
 scv.tl.velocity(adata_split2_seed317, mode="dynamical")
 scv.tl.velocity_graph(adata_split2_seed317)
 scv.pl.velocity_embedding_stream(adata_split2_seed317, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/scvelo_seed317_split2_2000.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_seed317_split2_2000.png")
 print("**************** seed317 split2 processed! ****************")
 
 # replace nan's to 0's in layers['velocity']
@@ -47,21 +60,27 @@ print("**************** seed317 cosine similarity computed! ****************")
 scv.pp.normalize_per_cell(adata_total)
 scv.pp.log1p(adata_total)
 scv.pp.moments(adata_total, n_pcs=30, n_neighbors=30)
-sc.tl.pca(adata_total, svd_solver="arpack")
+### batch correction
+bbknn.bbknn(adata_total, batch_key='sequencing.batch')
+adata_total.X = adata_total.X.toarray()
+bbknn.ridge_regression(adata_total, batch_key='sample', confounder_key='celltype')
+sc.tl.pca(adata_total)
+bbknn.bbknn(adata_total, batch_key='sequencing.batch')
+print("Batch correction done for total counts!")
 sc.pp.neighbors(adata_total, n_neighbors=10, n_pcs=40)
 sc.tl.umap(adata_total)
 scv.tl.recover_dynamics(adata_total)
 scv.tl.velocity(adata_total, mode="dynamical")
 scv.tl.velocity_graph(adata_total)
 scv.pl.velocity_embedding_stream(adata_total, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/scvelo_seed317_total_2000.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_seed317_total_2000.png")
 print("**************** total counts processed! ****************")
 
 # add cosine similarities to total counts object
 adata_total.obs["cos_sim_cell"] = np.diag(cos_sim)
 adata_total.obs["cos_sim_cell"] = pd.DataFrame(adata_total.obs["cos_sim_cell"])
 scv.pl.velocity_embedding_stream(adata_total, basis='umap',color="cos_sim_cell",cmap='coolwarm',
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/scvelo_seed317_cos_similarity_2000.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_seed317_cos_similarity_2000.png")
 print("**************** seed317 cosine similarity plotted! ****************")
 
 # read split counts data
@@ -73,28 +92,40 @@ print("**************** read seed320 split counts! ****************")
 scv.pp.normalize_per_cell(adata_split1_seed320)
 scv.pp.log1p(adata_split1_seed320)
 scv.pp.moments(adata_split1_seed320, n_pcs=30, n_neighbors=30)
-sc.tl.pca(adata_split1_seed320, svd_solver="arpack")
+### batch correction
+bbknn.bbknn(adata_split1_seed320, batch_key='sequencing.batch')
+adata_split1_seed320.X = adata_split1_seed320.X.toarray()
+bbknn.ridge_regression(adata_split1_seed320, batch_key='sample', confounder_key='celltype')
+sc.tl.pca(adata_split1_seed320)
+bbknn.bbknn(adata_split1_seed320, batch_key='sequencing.batch')
+print("Batch correction done for seed320 split1 counts!")
 sc.pp.neighbors(adata_split1_seed320, n_neighbors=10, n_pcs=40)
 sc.tl.umap(adata_split1_seed320)
 scv.tl.recover_dynamics(adata_split1_seed320)
 scv.tl.velocity(adata_split1_seed320, mode="dynamical")
 scv.tl.velocity_graph(adata_split1_seed320)
 scv.pl.velocity_embedding_stream(adata_split1_seed320, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/scvelo_seed320_split1_2000.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_seed320_split1_2000.png")
 print("**************** seed320 split1 processed! ****************")
 
 ## process split2
 scv.pp.normalize_per_cell(adata_split2_seed320)
 scv.pp.log1p(adata_split2_seed320)
 scv.pp.moments(adata_split2_seed320, n_pcs=30, n_neighbors=30)
-sc.tl.pca(adata_split2_seed320, svd_solver="arpack")
+### batch correction
+bbknn.bbknn(adata_split2_seed320, batch_key='sequencing.batch')
+adata_split2_seed320.X = adata_split2_seed320.X.toarray()
+bbknn.ridge_regression(adata_split2_seed320, batch_key='sample', confounder_key='celltype')
+sc.tl.pca(adata_split2_seed320)
+bbknn.bbknn(adata_split2_seed320, batch_key='sequencing.batch')
+print("Batch correction done for seed320 split2 counts!")
 sc.pp.neighbors(adata_split2_seed320, n_neighbors=10, n_pcs=40)
 sc.tl.umap(adata_split2_seed320)
 scv.tl.recover_dynamics(adata_split2_seed320)
 scv.tl.velocity(adata_split2_seed320, mode="dynamical")
 scv.tl.velocity_graph(adata_split2_seed320)
 scv.pl.velocity_embedding_stream(adata_split2_seed320, basis='umap',color="celltype",
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/scvelo_seed320_split2_2000.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_seed320_split2_2000.png")
 print("**************** seed320 split2 processed! ****************")
 
 # replace nan's to 0's in layers['velocity']
@@ -111,7 +142,7 @@ del adata_total.obs["cos_sim_cell"]
 adata_total.obs["cos_sim_cell"] = np.diag(cos_sim)
 adata_total.obs["cos_sim_cell"] = pd.DataFrame(adata_total.obs["cos_sim_cell"])
 scv.pl.velocity_embedding_stream(adata_total, basis='umap',color="cos_sim_cell",cmap='coolwarm',
-                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/scvelo_seed320_cos_similarity_2000.png")
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/erythroid/scvelo_seed320_cos_similarity_2000.png")
 print("**************** seed320 cosine similarity plotted! ****************")
 
 
