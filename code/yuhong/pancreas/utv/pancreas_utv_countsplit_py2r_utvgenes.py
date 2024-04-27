765ea61db458026d2651a7adcb11addd26776937
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-04-26 20:29:53 -0700

    utvgenes for countsplit py2r

---
+++ code/yuhong/pancreas/utv/pancreas_utv_countsplit_py2r_utvgenes.py
@@ -0,0 +1,37 @@
+import scvelo as scv
+import unitvelo as utv
+import scanpy as sc
+import tf_keras
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+
+### did not run pp.neighbors
+# the below script uses the environment: "utvClone"
+
+velo_config = utv.config.Configuration()
+velo_config.R2_ADJUST = False
+velo_config.IROOT = None
+velo_config.FIT_OPTION = '1'
+velo_config.ASSIGN_POS_U = True
+
+os.environ["TF_USE_LEGACY_KERAS"]="1"
+# https://github.com/tensorflow/tensorflow/issues/62337
+# To solve the following error: 
+## ImportError: `keras.optimizers.legacy` is not supported in Keras 3. 
+## When using `tf.keras`, to continue using a `tf.keras.optimizers.legacy` optimizer, 
+## you can install the `tf_keras` package (Keras 2) and set the environment variable `TF_USE_LEGACY_KERAS=True` 
+## to configure TensorFlow to use `tf_keras` when accessing `tf.keras`.
+# Tried this but did not work:
+## velo_config.TF_USE_LEGACY_KERAS=True
+
+label='clusters'
+adata = scv.read("/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/Pancreas/endocrinogenesis_day15.h5ad")
+
+path = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/Writeup4_utvgenes/tmp/pan_utv_py2r.h5ad"
+adata.write_h5ad(filename=path)
+adata_res = utv.run_model(adata, label, config_file=velo_config)
+adata_res.write_h5ad('/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/pan_utv_utvgenes/pan_utv_preprocess.h5ad')
+
+scv.pl.velocity_embedding_stream(adata_res,basis="umap",color="clusters",
+                                 save="/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/pancreas/unitvelo_utvgenes/unitvelo_preprocess.png")
+
