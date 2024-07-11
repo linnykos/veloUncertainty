90d460cd213d811ca7a447aa2fcf3895f4d75c86
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-07-10 23:04:52 -0700

    utv code

--- code/yuhong/erythroid/7_methods/sct/v2_ery_sct_2splitCorr.py
+++ code/yuhong/erythroid/7_methods/sct/v2_ery_sct_2splitCorr.py
@@ -10,14 +10,15 @@ import datetime
 from sklearn.metrics.pairwise import cosine_similarity
 
 data_folder = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/"
+fig_folder = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/v2_erythroid/" 
 
 # correlation between splits
-s1 = sc.read_h5ad(data_folder+'v2_erythroid/seed317_split1_allgenes.h5ad')
-s2 = sc.read_h5ad(data_folder+'v2_erythroid/seed317_split2_allgenes.h5ad')
+split1_allgenes = sc.read_h5ad(data_folder+'v2_erythroid/seed317_split1_allgenes.h5ad')
+split2_allgenes = sc.read_h5ad(data_folder+'v2_erythroid/seed317_split2_allgenes.h5ad')
 
 cor = []
-S1 = np.transpose(s1.layers['spliced'].todense())
-S2 = np.transpose(s2.layers['spliced'].todense())
+S1 = np.transpose(split1_allgenes.layers['spliced'].todense())
+S2 = np.transpose(split2_allgenes.layers['spliced'].todense())
 for i in range(S1.shape[0]):
     if i%1000==0:
         print(i)
@@ -29,18 +30,60 @@ cor = np.array(cor)
 cor[~np.isnan(cor)].shape
 np.quantile(cor[~np.isnan(cor)],[0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1])
 
+## plot correlation between splits (all in one color)
+plt.clf()
+plt.scatter(range(len(cor[~np.isnan(cor)])), cor[~np.isnan(cor)],color='royalblue',alpha=0.5)
+plt.title("Correlation of gene expr between splits (split_seed=317)")
+plt.xlabel("genes (with nonzero expr)")
+plt.ylabel("correlation")
+plt.savefig(fig_folder+"corr_between_splits_allgenes.png") 
+plt.clf()
+
 
 split1 = sc.read_h5ad(data_folder+'v2_erythroid/sct/adata_ery_sct_seed317_split1_v2.h5ad')
 split2 = sc.read_h5ad(data_folder+'v2_erythroid/sct/adata_ery_sct_seed317_split2_v2.h5ad')
 total = sc.read_h5ad(data_folder+'v2_erythroid/sct/adata_ery_sct_total_v2.h5ad')
 
+### common genes being selected and used for velocity computation
 common_genes = np.intersect1d(np.array(split1.var.index), np.array(split2.var.index))
-common_genes.shape # 1304 common genes
-np.intersect1d(np.array(split1.var.index), np.array(total.var.index)).shape # 229
-np.intersect1d(np.array(split2.var.index), np.array(total.var.index)).shape # 244
+common_genes.shape # 1304->1237 common genes
+np.intersect1d(np.array(split1.var.index), np.array(total.var.index)).shape # 229 -> 1531
+np.intersect1d(np.array(split2.var.index), np.array(total.var.index)).shape # 244 -> 1519
 
-velo_df1 = pd.DataFrame(split1.layers['velocity'], columns=split1.var.index.tolist())
-velo_df2 = pd.DataFrame(split2.layers['velocity'], columns=split2.var.index.tolist())
-cos_sim = np.diag(cosine_similarity(velo_df1[common_genes],velo_df2[common_genes]))
-np.quantile(cos_sim, [0,.5,1])
+# get the counts of commonly selected genes
+gene_names_split1 = split1.var.index.copy()
+positions_dict_split1 = {gene: pos for pos, gene in enumerate(gene_names_split1)}
+positions_split1 = [positions_dict_split1[gene] for gene in common_genes]
+
+gene_names_split2 = split2.var.index.copy()
+positions_dict_split2 = {gene: pos for pos, gene in enumerate(gene_names_split2)}
+positions_split2 = [positions_dict_split2[gene] for gene in common_genes]
+
+def compute_gene_correlation_between_splits(mat1,mat2):
+    m1 = np.transpose(mat1.todense())
+    m2 = np.transpose(mat2.todense())
+    cor = []
+    for i in range(m1.shape[0]):
+        if i%1000==0:
+            print(i)
+        if np.sum(m1[i,:])==0 and np.sum(m2[i,:])==0:
+            cor.append(np.nan)
+        else:
+            cor.append(np.corrcoef(m1[i,:],m2[i,:])[0,1])
+    cor = np.array(cor)
+    return cor
+
+# split1.layers['spliced'][:,positions_split1] is cell-by-gene, (9815, 1237)
+cor_sct = compute_gene_correlation_between_splits(split1.layers['spliced'][:,positions_split1],split2.layers['spliced'][:,positions_split2])
+cor_sct[~np.isnan(cor_sct)].shape
+np.quantile(cor_sct[~np.isnan(cor_sct)],[0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1])
+
+## plot correlation between splits (all in one color)
+plt.clf()
+plt.scatter(range(len(cor_sct[~np.isnan(cor_sct)])), cor_sct[~np.isnan(cor_sct)],color='royalblue',alpha=0.5)
+plt.title("Correlation of gene expr between splits (split_seed=317),Ngenes="+str(common_genes.shape[0]))
+plt.xlabel("genes")
+plt.ylabel("correlation")
+plt.savefig(fig_folder+"sct/split_cor/corr_between_splits_colorSame.png") 
+plt.clf()
 
