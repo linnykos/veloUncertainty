98867123ac156323dbc0b4f72a2d791bd8e2ec45
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-07-10 17:03:50 -0700

    ery scv all genes

--- code/yuhong/erythroid/7_methods/scv/v2_ery_scv_1data.py
+++ code/yuhong/erythroid/7_methods/scv/v2_ery_scv_1data.py
@@ -20,6 +20,11 @@ print_message_with_time("########### Start to read total, split1 and split2")
 total = sc.read_h5ad(data_folder+"Gastrulation/erythroid_lineage.h5ad")
 adata_split1 = sc.read_h5ad(data_folder+'v2_erythroid/seed317_split1_allgenes.h5ad') # 9815 × 53801
 adata_split2 = sc.read_h5ad(data_folder+'v2_erythroid/seed317_split2_allgenes.h5ad')
+gene_names = total.var.index.copy()
+S_mat_split1 = adata_split1.layers['spliced'].copy()
+S_mat_split2 = adata_split2.layers['spliced'].copy()
+U_mat_split1 = adata_split1.layers['unspliced'].copy()
+U_mat_split2 = adata_split2.layers['unspliced'].copy()
 
 def scv_compute_velocity(adata):
     scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
@@ -41,38 +46,96 @@ scv_compute_velocity(total) # running this
 scv_compute_velocity(adata_split1)
 scv_compute_velocity(adata_split2)
 
+positions_dict = {gene: pos for pos, gene in enumerate(gene_names)}
+positions_split1 = [positions_dict[gene] for gene in adata_split1.var.index]
+positions_split2 = [positions_dict[gene] for gene in adata_split2.var.index]
+
+S_mat_split1 = S_mat_split1[:,positions_split1]
+U_mat_split1 = U_mat_split1[:,positions_split1]
+adata_split1.layers['spliced_original'] = S_mat_split1[:,positions_split1] # looks like i did not do this actually
+adata_split1.layers['unspliced_original'] = U_mat_split1[:,positions_split1]
+adata_split2.layers['spliced_original'] = S_mat_split2[:,positions_split2]
+adata_split2.layers['unspliced_original'] = U_mat_split2[:,positions_split2]
+
+
 import numpy as np
 common_genes_filter = np.intersect1d(np.array(adata_split1.var.index), np.array(adata_split2.var.index))
-common_genes_filter.shape # 1499 common genes
-np.intersect1d(np.array(adata_split1.var.index), np.array(total.var.index)).shape # 589
-np.intersect1d(np.array(adata_split2.var.index), np.array(total.var.index)).shape # 580
+common_genes_filter.shape # 1481 common genes
+np.intersect1d(np.array(adata_split1.var.index), np.array(total.var.index)).shape # 589 -> 1335
+np.intersect1d(np.array(adata_split2.var.index), np.array(total.var.index)).shape # 580 -> 1328
 
 #adata_split1.layers["velocity_rmNA"] = np.nan_to_num(adata_split1.layers['velocity'], nan=0)
 #adata_split2.layers["velocity_rmNA"] = np.nan_to_num(adata_split2.layers['velocity'], nan=0)
-np.sum(~np.isnan(adata_split1.layers['velocity'][0])) # Ngenes in split1 for velocity computation=9
-np.sum(~np.isnan(adata_split2.layers['velocity'][0])) # Ngenes in split2 for velocity computation=9
+np.sum(~np.isnan(adata_split1.layers['velocity'][0])) # Ngenes in split1 for velocity computation=9 -> 311
+np.sum(~np.isnan(adata_split2.layers['velocity'][0])) # Ngenes in split2 for velocity computation=9 -> 315
 
 velo_genes_split1 = adata_split1.var.index[~np.isnan(adata_split1.layers['velocity'][0])]
 velo_genes_split2 = adata_split2.var.index[~np.isnan(adata_split2.layers['velocity'][0])]
 common_genes_velocity = np.intersect1d(np.array(velo_genes_split1), np.array(velo_genes_split2))
-common_genes_velocity.shape # =1
+common_genes_velocity.shape # =1 -> 209
 
 # write data
 total.write_h5ad(data_folder+'v2_erythroid/scv/adata_ery_scv_total_v2.h5ad')
 adata_split1.write_h5ad(data_folder+'v2_erythroid/scv/adata_ery_scv_seed317_split1_v2.h5ad')
 adata_split2.write_h5ad(data_folder+'v2_erythroid/scv/adata_ery_scv_seed317_split2_v2.h5ad')
 
+######################################################
+## plot velocities
+raw = sc.read_h5ad(data_folder+"Gastrulation/erythroid_lineage.h5ad")
+scv.pl.velocity_embedding_stream(total, basis='umap',color="celltype",save=fig_folder+"velocity/ery_scv_total_umapCompute.png")
+scv.pl.velocity_embedding_stream(total, basis='umap',color="sequencing.batch",save=fig_folder+"velocity/ery_scv_total_umapCompute_byBatch.png")
+
+def plot_velocities_scv(split, adata_total, fig_info):
+    adata=split.copy()
+    scv.pl.velocity_embedding_stream(adata, basis='umap',color="celltype",save=fig_folder+"velocity/ery_scv_"+fig_info+"_umapCompute.png")
+    adata.obsm['X_umap'] = adata_total.obsm['X_umap'].copy()
+    scv.pl.velocity_embedding_stream(adata, basis='umap',color="celltype",save=fig_folder+"velocity/ery_scv_"+fig_info+"_umapOriginal.png")
+
+plot_velocities_scv(total,raw,"total")
+plot_velocities_scv(adata_split1,raw,"seed317_split1")
+plot_velocities_scv(adata_split2,raw,"seed317_split2")
+
+
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 velo_df1 = pd.DataFrame(adata_split1.layers['velocity'], columns=adata_split1.var.index.tolist())
 velo_df2 = pd.DataFrame(adata_split2.layers['velocity'], columns=adata_split2.var.index.tolist())
 cos_sim = np.diag(cosine_similarity(velo_df1[common_genes_velocity],velo_df2[common_genes_velocity]))
+np.quantile(cos_sim,[0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1])
 
-##### copied from an earlier script
-scv.tl.rank_velocity_genes(adata, groupby='celltype', min_corr=.3)
-scv.tl.score_genes_cell_cycle(adata)
-scv.tl.velocity_confidence(adata)
-scv.tl.velocity_pseudotime(adata)
+# will be different
+#cs1=np.diag(cosine_similarity(np.nan_to_num(adata_split1.layers['velocity'], nan=0),np.nan_to_num(adata_split2.layers['velocity'], nan=0)))
+#np.quantile(cs1,[0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1])
 
+import matplotlib.pyplot as plt
+# cosine similarity
+def plot_cosine_similarity_scv(cos_sim, total, seed_split=317,text_x=None,text_y=None):
+    # histogram
+    plt.clf()
+    counts, bins, patches = plt.hist(cos_sim, bins=30, edgecolor='dimgray',color='powderblue') 
+    max_frequency = np.max(counts)
+    if text_x is None:
+        text_x = np.quantile(cos_sim,[.05])[0]
+    if text_y is None:
+        text_y = max_frequency/2
+    plt.axvline(np.mean(cos_sim), color='salmon', linestyle='dashed', linewidth=1.5) ## add mean
+    plt.text(text_x, text_y, 'mean = '+str(np.round(np.mean(cos_sim),5)), color='navy', fontsize=11)
+    plt.xlabel('cosine similarity (seed'+str(seed_split)+')')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of cosine similarity, ery+scv')
+    plt.savefig(fig_folder+'cos_sim/seed'+str(seed_split)+'_cos_sim_hist.png')
+    plt.clf()
+    # umap
+    total.obs['cos_sim'] = cos_sim
+    scv.pl.velocity_embedding_stream(total, basis='umap',color="cos_sim",cmap='coolwarm',perc=[1, 100],
+                                 save=fig_folder+"cos_sim/seed"+str(seed_split)+"_cos_sim_umapCompute.png")
+
+plot_cosine_similarity_scv(cos_sim,total)
+
+scv.tl.velocity_confidence(total)
+scv.pl.scatter(total, c='velocity_confidence', cmap='coolwarm', perc=[1, 100],
+               save=fig_folder+"velo_conf/total_veloConf_umapCompute.png")
+scv.pl.scatter(total, color='cos_sim', cmap='coolwarm', perc=[1, 100],
+               save=fig_folder+"cos_sim/seed317_cos_sim_umapCompute_scatter.png")
 
 
