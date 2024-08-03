b275442b6dd416ea55a0ef1713b06d9a7281aeee
Yuhong Li <wakeup@Wakeups-MacBook-Pro.local>
2024-08-02 19:50:50 -0700

    ery velovi woprep plots

--- code/yuhong/erythroid/7_methods/velovi/v2_ery_velovi_3plots_wopreprocess.py
+++ code/yuhong/erythroid/7_methods/velovi/v2_ery_velovi_3plots_wopreprocess.py
@@ -21,7 +21,7 @@ dataset_long = 'erythroid'
 data_folder = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/"
 fig_folder = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/v2_"+dataset_long+"/"+method+"/wopreprocess/"
 
-
+"""
 split1 = sc.read_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adata_"+dataset_short+"_"+method+"_split1_wopreprocess_v2.h5ad")
 vae_split1 = VELOVI.load(data_folder+"v2_"+dataset_long+"/"+method+'/wopreprocess/vae_ery_velovi_split1_wopreprocess_v2.pt', split1)
 
@@ -59,7 +59,7 @@ compute_umap_ery(total)
 split1.write_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adata_"+dataset_short+"_"+method+"_split1_wopreprocess_outputAdded_v2.h5ad")
 split2.write_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adata_"+dataset_short+"_"+method+"_split2_wopreprocess_outputAdded_v2.h5ad")
 total.write_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adata_"+dataset_short+"_"+method+"_total_wopreprocess_outputAdded_v2.h5ad")
-
+"""
 split1=sc.read_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adata_"+dataset_short+"_"+method+"_split1_wopreprocess_outputAdded_v2.h5ad")
 split2=sc.read_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adata_"+dataset_short+"_"+method+"_split2_wopreprocess_outputAdded_v2.h5ad")
 total=sc.read_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adata_"+dataset_short+"_"+method+"_total_wopreprocess_outputAdded_v2.h5ad")
@@ -67,6 +67,10 @@ total=sc.read_h5ad(data_folder+"v2_"+dataset_long+"/"+method+"/wopreprocess/adat
 vae_split1 = VELOVI.load(data_folder+"v2_"+dataset_long+"/"+method+'/wopreprocess/vae_ery_velovi_split1_wopreprocess_v2.pt', split1)
 vae_split2 = VELOVI.load(data_folder+"v2_"+dataset_long+"/"+method+'/wopreprocess/vae_ery_velovi_split2_wopreprocess_v2.pt', split2)
 vae_total = VELOVI.load(data_folder+"v2_"+dataset_long+"/"+method+'/wopreprocess/vae_ery_velovi_total_wopreprocess_v2.pt', total)
+
+vae_split1.adata = split1
+vae_split2.adata = split2
+vae_total.adata = total
 
 raw = sc.read_h5ad(data_folder+"Gastrulation/erythroid_lineage.h5ad")
 
