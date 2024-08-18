03300cea6579feb353d3c10e82ff3aa3553d7485
linnykos <kzlin@uw.edu>
2024-08-17 22:08:45 -0700

    Pushing the shuffle all experiment

--- code/kevin/Writeup9_sctour/Writeup9_sctour_larry_shuffle-all.py
+++ code/kevin/Writeup9_sctour/Writeup9_sctour_larry_shuffle-all.py
@@ -7,6 +7,7 @@ import torch
 import random
 import anndata as ad
 import datetime
+from scipy.sparse import csr_matrix
 
 sct_seed = 615
 
@@ -20,16 +21,46 @@ torch.manual_seed(sct_seed)
 random.seed(sct_seed)
 np.random.seed(sct_seed)
 
+################
+
+# Check if the layers exist before trying to delete them
+layers_to_remove = ['spliced', 'unspliced', 'ambiguous', 'matrix']
+
+for layer in layers_to_remove:
+    if layer in adata.layers:
+        del adata.layers[layer]
+
+# Verify the layers have been removed
+print(adata.layers)
+
+################
+
+# shuffle the genes 
+
+# Assuming adata.X is in a sparse matrix format, convert it to a dense matrix
+adata.X = adata.X.toarray()  # Convert to dense matrix if it's sparse
+
+# Shuffle values independently for each gene (column)
+for i in range(adata.X.shape[1]):
+    np.random.shuffle(adata.X[:, i])
+
+# If you want to convert back to a sparse matrix, you can do so (optional)
+adata.X = csr_matrix(adata.X)
+
+################
+
 adata.X = adata.X.astype(np.float32)
 sc.pp.calculate_qc_metrics(adata, 
                            percent_top=None, 
                            log1p=False, 
                            inplace=True)
-sc.pp.highly_variable_genes(adata, flavor='seurat_v3', 
+sc.pp.highly_variable_genes(adata, 
+                            flavor='seurat_v3', 
                             n_top_genes=2000, 
                             subset=True)
 
-tnode = sct.train.Trainer(adata, loss_mode='nb', 
+tnode = sct.train.Trainer(adata, 
+                          loss_mode='nb', 
                           alpha_recon_lec=0.5, 
                           alpha_recon_lode=0.5)
 tnode.train()
@@ -40,8 +71,8 @@ adata.obsm['X_TNODE'] = mix_zs
 adata.obsm['X_VF'] = tnode.get_vector_field(adata.obs['ptime'].values, 
                                             adata.obsm['X_TNODE'])
 
-adata.write("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup0_sctour_larry.h5ad")
+adata.write("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup0_sctour_larry_shuffle-all.h5ad")
 tnode.save_model(save_dir="/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/", 
-                 save_prefix="Writeup9_sctour_larry_tnode")
+                 save_prefix="Writeup9_sctour_larry_tnode_shuffle-all")
 print_message_with_time("########### Total data wrote")
 
