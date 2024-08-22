47ef22092e84d01590973c9a5448743b89bb63b7
linnykos <kzlin@uw.edu>
2024-08-21 22:04:09 -0700

    Updating all the scripts

--- code/kevin/Writeup9_sctour/Writeup9_larry_shuffle-all_split.py
+++ code/kevin/Writeup9_sctour/Writeup9_larry_shuffle-all_split.py
@@ -28,7 +28,7 @@ np.random.seed(sct_seed)
 ################
 
 # Check if the layers exist before trying to delete them
-layers_to_remove = ['spliced', 'unspliced', 'ambiguous', 'matrix']
+layers_to_remove = ['ambiguous', 'matrix']
 
 for layer in layers_to_remove:
     if layer in adata.layers:
@@ -68,13 +68,20 @@ sc.pp.highly_variable_genes(adata,
 # now split
 # code from: https://github.com/linnykos/veloUncertainty/blob/main/code/yuhong/larry/allgenes_countsplit.py
 
-X_mat = adata.X.copy()
-overdisps = estimate_overdisps(X_mat)
+S_mat = adata.layers['spliced'].copy()
+U_mat = adata.layers['unspliced'].copy()
+
+overdisps_S = estimate_overdisps(S_mat)
+overdisps_U = estimate_overdisps(U_mat)
 
 np.random.seed(sct_seed)
-X_split1, X_split2  = countsplit(X_mat,overdisps=overdisps)
+S_split1, S_split2  = countsplit(S_mat,overdisps=overdisps_S)
+np.random.seed(sct_seed)
+U_split1, U_split2  = countsplit(U_mat,overdisps=overdisps_U)
 
-adata_split1 = ad.AnnData(X=X_split1.astype(np.float32))
+adata_split1 = ad.AnnData(X=S_split1.astype(np.float32))
+adata_split1.layers["spliced"] = S_split1
+adata_split1.layers["unspliced"] = U_split1
 adata_split1.obs = pd.DataFrame(index=adata.obs.index)
 for obs_col in adata.obs.columns:
     adata_split1.obs[obs_col] = adata.obs[obs_col].copy()
@@ -85,7 +92,9 @@ for var_col in adata.var.columns:
 
 ###
 
-adata_split2 = ad.AnnData(X=X_split2.astype(np.float32))
+adata_split2 = ad.AnnData(X=S_split2.astype(np.float32))
+adata_split2.layers["spliced"] = S_split2
+adata_split2.layers["unspliced"] = U_split2
 adata_split2.obs = pd.DataFrame(index=adata.obs.index)
 for obs_col in adata.obs.columns:
     adata_split2.obs[obs_col] = adata.obs[obs_col].copy()
@@ -94,42 +103,6 @@ adata_split2.var = pd.DataFrame(index=adata.var.index)
 for var_col in adata.var.columns:
     adata_split2.var[var_col] = adata.var[var_col].copy()
 
-################
-
-# first train on split 1
-
-tnode_split1 = sct.train.Trainer(adata_split1, 
-                                 loss_mode='nb', 
-                                 alpha_recon_lec=0.5, 
-                                 alpha_recon_lode=0.5)
-tnode_split1.train()
-adata_split1.obs['ptime'] = tnode_split1.get_time()
-mix_zs, zs, pred_zs = tnode_split1.get_latentsp(alpha_z=0.5, 
-                                                alpha_predz=0.5)
-adata_split1.obsm['X_TNODE'] = mix_zs
-adata_split1.obsm['X_VF'] = tnode_split1.get_vector_field(adata_split1.obs['ptime'].values, 
-                                                          adata_split1.obsm['X_TNODE'])
-
-adata_split1.write("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup0_sctour_larry_shuffle-all_split1.h5ad")
-tnode_split1.save_model(save_dir="/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/", 
-                        save_prefix="Writeup9_sctour_larry_tnode_shuffle-all_split1")
-print_message_with_time("########### Split 1 wrote")
-
-# train on split 2
-
-tnode_split2 = sct.train.Trainer(adata_split2, 
-                                 loss_mode='nb', 
-                                 alpha_recon_lec=0.5, 
-                                 alpha_recon_lode=0.5)
-tnode_split2.train()
-adata_split2.obs['ptime'] = tnode_split2.get_time()
-mix_zs, zs, pred_zs = tnode_split2.get_latentsp(alpha_z=0.5, 
-                                                alpha_predz=0.5)
-adata_split2.obsm['X_TNODE'] = mix_zs
-adata_split2.obsm['X_VF'] = tnode_split2.get_vector_field(adata_split2.obs['ptime'].values, 
-                                                          adata_split2.obsm['X_TNODE'])
-
-adata_split2.write("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup0_sctour_larry_shuffle-all_split2.h5ad")
-tnode_split2.save_model(save_dir="/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/", 
-                        save_prefix="Writeup9_sctour_larry_tnode_shuffle-all_split2")
-print_message_with_time("########### Split 2 wrote")
+adata_split1.write("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup9_larry_shuffle-all_split1.h5ad")
+adata_split2.write("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup9_larry_shuffle-all_split2.h5ad")
+print_message_with_time("########### Finish splitting")
