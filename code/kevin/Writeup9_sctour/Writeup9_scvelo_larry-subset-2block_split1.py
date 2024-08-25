a2e0acd8d554ff1ff1149b4ad367de61b3473849
linnykos <kzlin@uw.edu>
2024-08-24 18:31:34 -0700

    Adding a lot of new scripts

---
+++ code/kevin/Writeup9_sctour/Writeup9_scvelo_larry-subset-2block_split1.py
@@ -0,0 +1,33 @@
+# scvelo virtual environment
+
+import scvelo as scv
+import scanpy as sc
+import os
+import datetime
+import random
+import numpy as np
+
+sct_seed = 615
+
+def print_message_with_time(message):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{message} at {current_time}")
+
+adata = sc.read_h5ad("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup9_larry-subset-2block_split1.h5ad")
+
+random.seed(sct_seed)
+np.random.seed(sct_seed)
+
+scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
+sc.tl.pca(adata, svd_solver="arpack")
+sc.pp.neighbors(adata, n_neighbors=15, n_pcs=40) # used to be n_neighbors=10
+sc.tl.umap(adata)
+scv.tl.recover_dynamics(adata)
+scv.tl.velocity(adata, mode="dynamical")
+scv.tl.velocity_graph(adata)
+
+print_message_with_time("########### Saving")
+adata.write("/home/users/kzlin/kzlinlab/projects/veloUncertainty/out/kevin/Writeup9/Writeup9_scvelo_larry-subset-2block_split1.h5ad")
+print_message_with_time("########### Total data wrote")
+
