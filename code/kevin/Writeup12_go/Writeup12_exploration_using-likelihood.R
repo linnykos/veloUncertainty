4f695aa1ea3fdc02ff058f12384da1e5d12844fd
linnykos <kzlin@uw.edu>
2024-10-28 22:35:30 -0700

    Pushing latest things

--- code/kevin/Writeup12_go/Writeup12_exploration_using-likelihood.R
+++ code/kevin/Writeup12_go/Writeup12_exploration_using-likelihood.R
@@ -20,23 +20,25 @@ df_subset <- df[,lik_idx]
 median_lik <- apply(df_subset, 1, function(x){stats::median(x, na.rm = TRUE)})
 names(median_lik) <- df[,"gene_name"]
 
-teststat_vec <- num_velocity_var
-teststat_vec <- sort(teststat_vec, decreasing = TRUE)
-keep_genes <- intersect(names(teststat_vec)[teststat_vec != 0],
-                        names(median_lik)[!is.na(median_lik)])
-teststat_vec <- teststat_vec[keep_genes]
-median_lik <- median_lik[keep_genes]
+# teststat_vec <- num_velocity_var
+# teststat_vec <- sort(teststat_vec, decreasing = TRUE)
+# keep_genes <- intersect(names(teststat_vec)[teststat_vec != 0],
+#                         names(median_lik)[!is.na(median_lik)])
+# teststat_vec <- teststat_vec[keep_genes]
+# median_lik <- median_lik[keep_genes]
+# 
+# # break ties in teststat_vec based on the higher likelihood
+# uniq_values <- sort(unique(teststat_vec))
+# for(val in uniq_values){
+#   idx <- which(teststat_vec == val)
+#   median_lik_subset <- median_lik[idx]
+#   median_lik_subset <- sort(median_lik_subset, decreasing = FALSE)
+#   gene_order <- names(median_lik_subset)
+#   teststat_vec[gene_order] <- teststat_vec[gene_order] + seq(0, 0.95, length.out = length(gene_order))
+# }
 
-# break ties in teststat_vec based on the higher likelihood
-uniq_values <- sort(unique(teststat_vec))
-for(val in uniq_values){
-  idx <- which(teststat_vec == val)
-  median_lik_subset <- median_lik[idx]
-  median_lik_subset <- sort(median_lik_subset, decreasing = FALSE)
-  gene_order <- names(median_lik_subset)
-  teststat_vec[gene_order] <- teststat_vec[gene_order] + seq(0, 0.95, length.out = length(gene_order))
-}
-
+teststat_vec <- median_lik
+teststat_vec <- teststat_vec[!is.na(teststat_vec)]
 teststat_vec <- sort(teststat_vec, decreasing = TRUE)
 
 # https://github.com/YuLab-SMU/clusterProfiler/issues/307
@@ -54,7 +56,8 @@ gse <- clusterProfiler::gseGO(
 
 gse_df <- as.data.frame(gse)
 
-gse_df[which(gse_df$p.adjust <= 0.1), c("Description","NES","p.adjust")]
+gse_df[which(gse_df$p.adjust <= 0.05), c("Description", "NES", "p.adjust")]
+# I don't like these as much... too many neuron things...
 
 write.csv(gse_df, 
           file = paste0(csv_folder, "Writeup12_pancreas_scvelo_GSEA.csv"))
