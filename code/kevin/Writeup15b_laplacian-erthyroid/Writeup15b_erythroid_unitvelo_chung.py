15bfa0a32605703c74c82d18690f9de0fe7eda1c
linnykos <kzlin@uw.edu>
2025-06-13 15:56:33 -0700

    Updating the code

--- code/kevin/Writeup15b_laplacian-erthyroid/Writeup15b_erythroid_unitvelo_chung.py
+++ code/kevin/Writeup15b_laplacian-erthyroid/Writeup15b_erythroid_unitvelo_chung.py
@@ -59,8 +59,13 @@ for gene_idx in range(n_genes):
     print(f"Working on gene {gene_idx + 1} out of {n_genes}", flush=True)
     
     # ---- numerator  xᵀ L x  -------------------------------------------
-    x_dense = np.asarray(adata2[:, gene_idx].X).ravel()   # 1-D vector
-    y       = laplacian.dot(x_dense)                      # L x
+    vec = adata2[:, gene_idx].X         
+    if sp.issparse(vec):
+        x_dense = vec.toarray().ravel()  
+    else:
+        x_dense = np.asarray(vec).ravel()
+    
+    y = laplacian.dot(x_dense)                      # L x
     numer   = x_dense.dot(y)                              # xᵀ L x
     
     nonzero_mask = x_dense != 0
@@ -73,7 +78,7 @@ for gene_idx in range(n_genes):
     pi_nz_sum   = pi[nonzero_mask].sum()
     pi_zero_sum = 1.0 - pi_nz_sum
     
-    var_pi = np.dot(pi[nonzero_mask], (data - mean_pi) ** 2) + pi_zero_sum * (mean_pi ** 2)
+    var_pi = np.dot(pi[nonzero_mask], (x_dense[nonzero_mask] - mean_pi) ** 2) + pi_zero_sum * (mean_pi ** 2)
     
     # guard against genes that are constant under π
     if var_pi < 1e-12:
