76f6cfdfe214e4320ec638b48a33fdcb9605a6dc
Yuhong <118783039+liyu5z@users.noreply.github.com>
2025-08-13 11:40:16 -0700

    Update velovi_2plots.py

--- code/pancreasINC/seeds/method_velovi/velovi_2plots.py
+++ code/pancreasINC/seeds/method_velovi/velovi_2plots.py
@@ -66,24 +66,6 @@ def velovi_panINC_plots(split_seed):
     plot_veloConf_hist(total,dataset_short,method,fig_folder,split_seed)
     plot_velo_conf_boxplot_by_celltype(total,dataset_short,method,fig_folder,split_seed)
     print('correlation of velocity confidence between splits='+str(np.round(np.corrcoef(split1.obs['velocity_confidence'],split2.obs['velocity_confidence']),5)))
-    ## ptime
-    print_message_with_time(str(split_seed)+': velocity pseudotime plots') 
-    scv.tl.velocity_pseudotime(total,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split1,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split2,use_velocity_graph=False)
-    plot_pseudotime(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-    plot_pseudotime(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-    plot_pseudotime(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-    ptime_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,time_label='velocity_pseudotime',split_seed=split_seed)
-    # latent time
-    print_message_with_time(str(split_seed)+': latent time plots')
-    compute_latent_time(total)
-    compute_latent_time(split1)
-    compute_latent_time(split2)
-    plot_latent_time(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-    plot_latent_time(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-    plot_latent_time(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-    latent_time_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,split_seed=split_seed)
     # shuffled cosine similarity
     print_message_with_time(str(split_seed)+': shuffled cosine similarity')
     v2s_mean,v2s_median = compute_cosine_similarity_shuffled(split1,split2,method=method,seed=1508)
@@ -101,22 +83,7 @@ def velovi_panINC_plots(split_seed):
     print(np.quantile(c2,[0.,.25,.5,.75,1.]) )
     print_message_with_time('####################### seed'+str(split_seed)+': all done! #######################') 
 
-
 velovi_panINC_plots(split_seed=320)
 velovi_panINC_plots(split_seed=323)
 velovi_panINC_plots(split_seed=326)
 velovi_panINC_plots(split_seed=329)
-
-
-
-
-
-"""
-vae_split1 = VELOVI.load(data_folder+'v4_'+dataset_long+'/seed'+str(split_seed)+'/'+method+'/vae_'+dataset_short+'_'+method+'_split1_v4.pt', split1)
-vae_split2 = VELOVI.load(data_folder+'v4_'+dataset_long+'/seed'+str(split_seed)+'/'+method+'/vae_'+dataset_short+'_'+method+'_split2_v4.pt', split2)
-vae_total = VELOVI.load(data_folder+'v4_'+dataset_long+'/seed'+str(split_seed)+'/'+method+'/vae_'+dataset_short+'_'+method+'_total_v4.pt', total)
-"""
-
-
-
-
