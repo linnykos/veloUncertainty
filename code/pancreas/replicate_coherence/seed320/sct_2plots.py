424a6a47d0742d97d8a03955249cb3eb48c58c25
Yuhong <118783039+liyu5z@users.noreply.github.com>
2025-08-13 11:31:13 -0700

    Update sct_2plots.py

--- code/pancreas/replicate_coherence/seed320/sct_2plots.py
+++ code/pancreas/replicate_coherence/seed320/sct_2plots.py
@@ -90,36 +90,6 @@ def plot_pan_sct(split_seed):
     print('correlation of velocity confidence')
     print(np.corrcoef(split1.obs['velocity_confidence'],split2.obs['velocity_confidence']) )
 
-    ######################################################
-    """
-    ## ptime
-    scv.tl.velocity_pseudotime(total,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split1,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split2,use_velocity_graph=False)
-
-    plot_pseudotime(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-    plot_pseudotime(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-    plot_pseudotime(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-
-    ptime_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,time_label='velocity_pseudotime',split_seed=split_seed)
-    # -0.463 -> -0.463
-
-    if not 'latent_time' in split1.obs.columns:
-        scv.tl.recover_dynamics(total,n_jobs=8)
-        scv.tl.latent_time(total)
-        plot_latent_time(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-        scv.tl.recover_dynamics(split1,n_jobs=8)
-        scv.tl.latent_time(split1)
-        plot_latent_time(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-        scv.tl.recover_dynamics(split2,n_jobs=8)
-        scv.tl.latent_time(split2)
-        plot_latent_time(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-
-    latent_time_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,split_seed=split_seed)
-
-    print_ptime_corr_by_celltype(split1,split2,total,dataset_short,ptime_label='velocity_pseudotime')
-    print_ptime_corr_by_celltype(split1,split2,total,dataset_short,ptime_label='ptime')
-    """
     ###################################
     # method-selected gene corr
     plot_method_gene_corr(split1, split2, method, dataset_short, fig_folder, split_seed)
