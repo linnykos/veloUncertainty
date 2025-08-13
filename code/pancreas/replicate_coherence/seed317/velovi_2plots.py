55589b6999d3003c845352f6ded9191d8bd493be
Yuhong <118783039+liyu5z@users.noreply.github.com>
2025-08-13 11:28:59 -0700

    Update velovi_2plots.py

--- code/pancreas/replicate_coherence/seed317/velovi_2plots.py
+++ code/pancreas/replicate_coherence/seed317/velovi_2plots.py
@@ -22,7 +22,6 @@ dataset_long = 'pancreas'
 data_folder = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/"
 fig_folder = '/home/users/y2564li/kzlinlab/projects/veloUncertainty/git/veloUncertainty/fig/yuhong/v4_'+dataset_long+'/seed'+str(split_seed)+'/'+method+'/'
 
-"""
 split1 = read_data_v4(dataset_long,dataset_short,method,split_seed,data_version='split1',allgenes=False,outputAdded=False)
 vae_split1 = VELOVI.load(data_folder+'v4_'+dataset_long+'/seed'+str(split_seed)+'/'+method+'/vae_'+dataset_short+'_'+method+'_split1_v4.pt', split1)
 
@@ -53,8 +52,8 @@ split1.write_h5ad(data_folder+'v4_'+dataset_long+'/seed'+str(split_seed)+'/'+met
 split2.write_h5ad(data_folder+'v4_'+dataset_long+'/seed'+str(split_seed)+'/'+method+'/adata_'+dataset_short+'_'+method+'_split2_v4_outputAdded.h5ad')
 total.write_h5ad(data_folder+'v4_'+dataset_long+'/seed'+str(split_seed)+'/'+method+'/adata_'+dataset_short+'_'+method+'_total_v4_outputAdded.h5ad')
 
-"""
 
+## read the outputAdded version
 split1 = read_data_v4(dataset_long,dataset_short,method,split_seed,data_version='split1',allgenes=False,outputAdded=True)
 split2 = read_data_v4(dataset_long,dataset_short,method,split_seed,data_version='split2',allgenes=False,outputAdded=True)
 total = read_data_v4(dataset_long,dataset_short,method,split_seed,data_version='total',allgenes=False,outputAdded=True)
@@ -97,38 +96,7 @@ plot_veloConf_and_cosSim(total,split1,split2,dataset_short,method,fig_folder,spl
 plot_veloConf_hist(total,dataset_short,method,fig_folder,split_seed)
 plot_velo_conf_boxplot_by_celltype(total,dataset_short,method,fig_folder,split_seed)
 
-np.corrcoef(split1.obs['velocity_confidence'],split2.obs['velocity_confidence']) # 0.39564399
-
 ######################################################
-## ptime
-if not 'velocity_pseudotime' in split1.obs.columns:
-    scv.tl.velocity_pseudotime(total,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split1,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split2,use_velocity_graph=False)
-
-plot_pseudotime(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-plot_pseudotime(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-plot_pseudotime(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-
-ptime_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,time_label='velocity_pseudotime',split_seed=split_seed)
-# 0.833
-
-def compute_latent_time(adata):
-    scv.tl.recover_dynamics(adata,n_jobs=8)
-    scv.tl.velocity_graph(adata,n_jobs=8)
-    scv.tl.latent_time(adata)
-
-compute_latent_time(total)
-compute_latent_time(split1)
-compute_latent_time(split2)
-
-plot_latent_time(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-plot_latent_time(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-plot_latent_time(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed)
-
-latent_time_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,split_seed=split_seed)
-# 0.942
-
 # shuffled cosine similarity
 v2s_mean,v2s_median = compute_cosine_similarity_shuffled(split1,split2,method=method,seed=1508)
 np.round(np.mean(v2s_mean),4) # 
@@ -140,9 +108,3 @@ np.round(np.var(v2s_median),4) # 0.
 # paired: 0.378 0.3786
 # shuffled: 0.199 0.1688
 
-np.quantile([np.var(total.layers['velocity'][:,i]) for i in range(2000)],[0.,.25,.5,.75,1.])
-np.quantile([np.var(split1.layers['velocity'][:,i]) for i in range(2000)],[0.,.25,.5,.75,1.])
-np.quantile([np.var(split2.layers['velocity'][:,i]) for i in range(2000)],[0.,.25,.5,.75,1.])
-"""
-
-"""
