70b7dcd18f464cd42d273bb043bc20b682748139
y2564li <y2564li@biostat-bayes0.biostat.washington.edu>
2025-08-14 09:50:47 -0700

    overdispersion code update

---
+++ code/greenleaf/countsplit/v4_glf_overdisp-2.R
@@ -0,0 +1,12 @@
+library(Matrix)
+data_folder = "/home/users/y2564li/kzlinlab/projects/veloUncertainty/out/yuhong/data/"
+
+S <- readMM(paste0(data_folder,'v4_greenleaf/glf_spliced.mtx'))
+U <- readMM(paste0(data_folder,'v4_greenleaf/glf_unspliced.mtx'))
+
+overdisp_S <- sapply(1:ncol(S), function(i) 1/glmGamPoi::glm_gp(as.numeric(S[,i]), design = ~ 1)$overdispersions)
+overdisp_U <- sapply(1:ncol(U), function(i) 1/glmGamPoi::glm_gp(as.numeric(U[,i]), design = ~ 1)$overdispersions)
+
+write.csv(overdisp_S, file=paste0(data_folder,'v4_greenleaf/glf_overdisp_S.csv'))
+write.csv(overdisp_U, file=paste0(data_folder,'v4_greenleaf/glf_overdisp_U.csv'))
+
