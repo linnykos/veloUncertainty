cadbb5463e69a8a2cfcc6b270519f096015e444c
Yuhong <118783039+liyu5z@users.noreply.github.com>
2025-08-13 22:10:12 -0700

    Update velovi_4GPC_plots.py

--- code/greenleaf/method_velovi/velovi_4GPC_plots.py
+++ code/greenleaf/method_velovi/velovi_4GPC_plots.py
@@ -103,24 +103,3 @@ if (not 'velocity_confidence' in total.obs.columns):
 plot_veloConf_hist(total,dataset_short,method,fig_folder,split_seed)
 plot_velo_conf_boxplot_by_celltype(total,dataset_short,method,fig_folder,split_seed)
 
-######################################################
-## ptime
-"""
-print('######## pseudotime')
-if not 'velocity_pseudotime' in split1.obs.columns:
-    scv.tl.velocity_pseudotime(total,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split1,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split2,use_velocity_graph=False)
-
-plot_pseudotime(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-plot_pseudotime(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-plot_pseudotime(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-
-ptime_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,time_label='velocity_pseudotime',split_seed=split_seed)
-"""
-
-
-
-
-
-
