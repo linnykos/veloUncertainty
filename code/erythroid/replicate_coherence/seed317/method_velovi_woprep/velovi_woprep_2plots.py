4e55aa5416dd354911791b2ce445c8c44a45f694
Yuhong <118783039+liyu5z@users.noreply.github.com>
2025-08-13 22:14:20 -0700

    Update velovi_woprep_2plots.py

--- code/erythroid/replicate_coherence/seed317/method_velovi_woprep/velovi_woprep_2plots.py
+++ code/erythroid/replicate_coherence/seed317/method_velovi_woprep/velovi_woprep_2plots.py
@@ -100,36 +100,7 @@ plot_veloConf_and_cosSim(total,split1,split2,dataset_short,method,fig_folder,spl
 plot_veloConf_hist(total,dataset_short,method,fig_folder,split_seed)
 plot_velo_conf_boxplot_by_celltype(total,dataset_short,method,fig_folder,split_seed)
 
-np.corrcoef(split1.obs['velocity_confidence'],split2.obs['velocity_confidence']) # 0.43612837
-
 ######################################################
-## ptime
-if not 'velocity_pseudotime' in split1.obs.columns:
-    scv.tl.velocity_pseudotime(total,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split1,use_velocity_graph=False)
-    scv.tl.velocity_pseudotime(split2,use_velocity_graph=False)
-
-plot_pseudotime(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-plot_pseudotime(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-plot_pseudotime(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,ptime_label='velocity_pseudotime')
-
-ptime_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,time_label='velocity_pseudotime',split_seed=split_seed)
-# 0.839
-
-"""
-if not 'latent_time' in split1.obs.columns:
-    scv.tl.latent_time(total)
-    scv.tl.latent_time(split1)
-    scv.tl.latent_time(split2)
-
-# KeyError: 'fit_likelihood'
-plot_latent_time(adata_in=split1,data_version='split1',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,celltype_label='state_info')
-plot_latent_time(adata_in=split2,data_version='split2',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,celltype_label='state_info')
-plot_latent_time(adata_in=total,data_version='total',dataset=dataset_short,method=method,fig_folder=fig_folder,split_seed=split_seed,celltype_label='state_info')
-
-latent_time_correlation_scatter_spearman(s1=split1,s2=split2,method=method,dataset=dataset_short,name='split1vs2',xlab='split1',ylab='split2',fig_folder=fig_folder,split_seed=split_seed,celltype_label='state_info')
-"""
-
 # shuffled cosine similarity
 v2s_mean,v2s_median = compute_cosine_similarity_shuffled(split1,split2,method=method,seed=1508)
 np.round(np.mean(v2s_mean),4) # 0.8757
@@ -140,11 +111,3 @@ np.round(np.var(v2s_median),4)
 
 # paired: 0.9313 0.9454
 # shuffled: 0.8757 0.8994
-
-np.quantile([np.var(total.layers['velocity'][:,i]) for i in range(2000)],[0.,.25,.5,.75,1.])
-np.quantile([np.var(split1.layers['velocity'][:,i]) for i in range(2000)],[0.,.25,.5,.75,1.])
-np.quantile([np.var(split2.layers['velocity'][:,i]) for i in range(2000)],[0.,.25,.5,.75,1.])
-
-# [0.00000000e+00, 7.64297667e-07, 1.44990809e-05, 3.13360120e-04, 8.36765747e+01]
-# [0.00000000e+00, 3.34343170e-07, 3.48852461e-06, 3.42113917e-05, 3.72625694e+01]
-# [0.00000000e+00, 4.86066682e-07, 4.30251725e-06, 3.82065136e-05, 3.74937248e+01]
